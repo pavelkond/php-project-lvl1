085b96692544975b04d7e034a74a3095f84c5696
Pavel Kondybailo <pavelkondybailo@yandex.by>
2022-01-20 18:19:46 +0300

    Add Engine.php that contain logic of all games

---
+++ src/Engine.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Brain\Games\Engine;
+
+use function cli\line;
+use function cli\prompt;
+
+function getName(): string
+{
+    line("Welcome to the Brain Game!");
+    $name = prompt("May I have your name?");
+    line("Hello, %s!", $name);
+    return $name;
+}
+
+function playGame(string $gameRules, array $tasks)
+{
+    $playerName = getName();
+    line($gameRules);
+    foreach ($tasks as [$question, $answer]) {
+        line("Question: %s", $question);
+        $playerAnswer = prompt("Your answer");
+        if ($playerAnswer === (string) $answer) {
+            line("Correct!");
+        } else {
+            line("'%s' is wrong answer ;(. Correct answer was '%s'", $playerAnswer, $answer);
+            return;
+        }
+    }
+    line("Congratulations, %s!", $playerName);
+}
