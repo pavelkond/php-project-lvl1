8c163407c2bb1d4de4ebd682900e24cdf44d2eeb
Pavel Kondybailo <pavelkondybailo@yandex.by>
2022-01-29 18:00:51 +0300

    Update Prime.php

--- src/Prime.php
+++ src/Prime.php
@@ -6,7 +6,7 @@ use function Brain\Games\Engine\playGame;
 
 function isPrime(int $number): bool
 {
-    if ($number <= 2) {
+    if ($number < 2) {
         return false;
     }
     for ($i = 2; $i <= $number / 2; $i++) {
